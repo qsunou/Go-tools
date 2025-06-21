60673f07dfddf082162e6a9186b65deced7ba28f
qsunou <qsunou@gmail.com>
2025-06-21 20:31:42 +0800

    feat add constrain

--- slice/add.go
+++ slice/add.go
@@ -1,1 +1,10 @@
 package slice
+
+import (
+	"github.com/qsunou/Go-tools/internal/slice"
+)
+
+func Add[Src any](src []Src, element Src, index int) ([]Src, error) {
+	res, err := slice.Add[Src](src, element, index)
+	return res, err
+}
---
+++ slice/add_test.go
@@ -0,0 +1,57 @@
+package slice
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/qsunou/Go-tools/internal/errs"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	// Add mainly relies on internal/slice.Add to ensure correctness
+	testCases := []struct {
+		name      string
+		slice     []int
+		addVal    int
+		index     int
+		wantSlice []int
+		wantErr   error
+	}{
+		{
+			name:      "index 0",
+			slice:     []int{123, 100},
+			addVal:    233,
+			index:     0,
+			wantSlice: []int{233, 123, 100},
+		},
+		{
+			name:    "index -1",
+			slice:   []int{123, 100},
+			index:   -1,
+			wantErr: errs.NewErrIndexOutOfRange(2, -1),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := Add(tc.slice, tc.addVal, tc.index)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantSlice, res)
+		})
+	}
+}
+
+func ExampleAdd() {
+	res, _ := Add[int]([]int{1, 2, 3, 4}, 233, 2)
+	fmt.Println(res)
+	_, err := Add[int]([]int{1, 2, 3, 4}, 233, -1)
+	fmt.Println(err)
+	// Output:
+	// [1 2 233 3 4]
+	// ekit: index out of range, length 4, index -1
+}
